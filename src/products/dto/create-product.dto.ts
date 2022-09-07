7f83cd1094fa555807b2ee1d2f73bc880b52053f
pblgllgs <pbl.gllgs@gmail.com>
2022-09-06 23:01:25 -0300

    cacth exception requred fields, method

---
+++ src/products/dto/create-product.dto.ts
@@ -0,0 +1,43 @@
+import {
+  IsArray,
+  IsIn,
+  IsInt,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsPositive,
+  IsString,
+  MinLength,
+} from 'class-validator';
+
+export class CreateProductDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(1)
+  title: string;
+
+  @IsNumber()
+  @IsOptional()
+  @IsPositive()
+  price?: number;
+
+  @IsString()
+  @IsOptional()
+  description?: string;
+
+  @IsString()
+  @IsOptional()
+  slug?: string;
+
+  @IsPositive()
+  @IsInt()
+  @IsOptional()
+  stock?: number;
+
+  @IsString({ each: true })
+  @IsArray()
+  sizes: string[];
+
+  @IsIn(['men', 'women', 'kid', 'unisex'])
+  gender: string;
+}
