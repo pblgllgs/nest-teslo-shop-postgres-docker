7dd47d5f21960490254c1041a298880d4c900dfd
pblgllgs <pbl.gllgs@gmail.com>
2022-12-06 23:28:52 -0300

    update handleDBExceptions

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -131,10 +131,10 @@ export class ProductsService {
   }
 
   private handleDBExceptions(error: any): never {
+    this.logger.error(error);
     if (error.code === '23505') {
       throw new BadRequestException(error.detail);
     }
-    this.logger.error(error);
     throw new InternalServerErrorException('Check server logs');
   }
 
